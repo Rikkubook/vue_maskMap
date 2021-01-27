a9fe3aded5e8bb0b128117b6d3ec220e3270e9e8
林佩瑤 <rikkubook@gmail.com>
2021-01-27 18:27:07 +0800

    [develop 改標籤/側邊欄顯示1/2]

--- src/App.vue
+++ src/App.vue
@@ -3,42 +3,59 @@
     <div class="container-fluid">
       <div class="row flex-nowrap">
         <b-card
-          title="Card Title"
           img-src="https://picsum.photos/600/300/?image=25"
           img-alt="Image"
           img-top
           tag="article"
           class="ri-sideMenu"
         >
-          <b-form v-if="show">
-            <b-form-group id="input-group-3" label="城市:" label-for="input-city">
-              <!-- <b-form-select
-                id="input-city"
-                v-model="form.city"
-                :options="cityArray"
-                required
-                @change="removeMark();updateMap()"  //監聽改變
-              /> -->
-              <b-form-select
-                id="input-city"
-                v-model="form.city"
-                :options="cityArray"
-                required
-                @change="removeMark();updateMap()"
-              />
-            </b-form-group>
-            <b-form-group id="input-group-3" label="區域:" label-for="input-city">
-              <b-form-select
-                id="input-city"
-                v-model="form.area"
-                :options="areaArray"
-                required
-                @change="updateAreaMap()"
-              />
-            </b-form-group>
-
-            <b-button type="submit" variant="primary">Submit</b-button>
-          </b-form>
+          <div class="ri-sideMenu_search">
+            <h1 class="ri-sideMenu_search_title">
+              口罩地圖
+            </h1>
+            <b-form>
+              <b-form-group id="input-group-3" label="城市:" label-for="input-city">
+                <!-- <b-form-select
+                  id="input-city"
+                  v-model="form.city"
+                  :options="cityArray"
+                  required
+                  @change="removeMark();updateMap()"  //監聽改變
+                /> -->
+                <b-form-select
+                  id="input-city"
+                  v-model="form.city"
+                  :options="cityArray"
+                  required
+                  @change="removeMark();updateMap()"
+                />
+              </b-form-group>
+              <b-form-group id="input-group-3" label="區域:" label-for="input-city" class="mb-0">
+                <b-form-select
+                  id="input-city"
+                  v-model="form.area"
+                  :options="areaArray"
+                  required
+                  @change="updateAreaMap()"
+                />
+              </b-form-group>
+            </b-form>
+          </div>
+          <ul class="ri-sideMenu_list">
+            <li v-for="listItem in List" :key="listItem.properties.id">
+              <h2 class="ri-sideMenu_list_title">藥局名稱:</h2>
+              <p class="ri-sideMenu_list_note">口罩販賣時段：星期一~六09:00-11:00</p>
+              <p class="ri-sideMenu_list_address">
+                <span>地址: </span>新竹縣新豐鄉建興路二段８１４號
+              </p>
+              <p class="ri-sideMenu_list_openTime">
+                <span>營業時間: </span>
+              </p>
+              <p class="ri-sideMenu_list_phone">
+                <span>電話: </span>
+              </p>
+            </li>
+          </ul>
         </b-card>
         <div id="mapid"></div>
       </div>
@@ -50,7 +67,7 @@
 import L from 'leaflet'
 import TWcity from '@/assets/TWcity.json'
 import axios from 'axios'
-import { BCard, BForm, BFormGroup, BFormSelect, BButton } from 'bootstrap-vue'
+import { BCard, BForm, BFormGroup, BFormSelect } from 'bootstrap-vue'
 
 console.log(L)
 
@@ -58,7 +75,7 @@ let openStreetMap = {};
 
 export default {
   name: 'App',
-  components: { BCard, BForm, BFormGroup, BFormSelect, BButton },
+  components: { BCard, BForm, BFormGroup, BFormSelect },
   data () {
     return {
       form: {
@@ -70,7 +87,8 @@ export default {
       show: true,
       openStreetMap: {},
       Pharmacy: [],
-      PharmacyCity: []
+      PharmacyCity: [],
+      List: []
     }
   },
   mounted() {
@@ -120,7 +138,14 @@ export default {
       const pharmacies = this.Pharmacy.filter( pharmacyItem => pharmacyItem.properties.county == this.form.city)
       console.log('updateMap', pharmacies)
       this.PharmacyCity = pharmacies
+      
 
+      const greenIcon = L.icon({
+          iconUrl: '/marker-02.png',
+          iconSize:     [38, 50],
+          iconAnchor:   [25,60],
+          popupAnchor:  [0, -76]
+      })
       // 添加圖標
       pharmacies.forEach((pharmacyItem)=>{
         const { properties, geometry } =pharmacyItem // 可以用解構的方式比較不會那麼長
@@ -129,7 +154,9 @@ export default {
           // pharmacyItem.geometry.coordinates[0]
           geometry.coordinates[1],
           geometry.coordinates[0]
-        ]).addTo(openStreetMap)
+        ],{
+          icon: greenIcon
+        }).addTo(openStreetMap)
         .bindPopup(`
           <div class="ri-popup">
             <div class="ri-popup_title">藥局名稱: ${ properties.name }</div>
@@ -156,6 +183,7 @@ export default {
     updateAreaMap () {
       const pharmacies = this.PharmacyCity.filter( pharmacyItem => pharmacyItem.properties.town == this.form.area)
       console.log('updateArea', pharmacies)
+      this.List = pharmacies
 
       // 更新圖資位置(用取得的第一個藥局)
       this.moveTo(pharmacies[0])
@@ -185,16 +213,64 @@ export default {
 <style lang="scss">
 @import '@/assets/sass/all.scss';
 
-*{
-  outline: 1px solid red;
-}
-.ri-sideMenu{
-  width: 300px;
-  height: 100vh;
+// *{
+//   outline: 1px solid red;
+// }
+.card-body{
+  padding: 0px;
 }
 #mapid {
   width: 100%;
 }
+
+.ri-sideMenu{
+  width: 400px;
+  height: 100vh;
+}
+.ri-sideMenu_search{
+  padding: 20px 10px 20px;
+  margin-bottom: 5px;
+  .ri-sideMenu_search_title{
+    font-size: 24px;
+  }
+}
+
+.ri-sideMenu_list{
+  height: calc(100vh - 365px);
+  overflow-y: auto;
+  padding: 10px;
+  background-color: #ffffff;
+  list-style: none;
+  li{
+    background-color: #DFEAEC;
+    box-shadow: 0px 2px 5px rgba(0,0,0,0.2);
+    margin-bottom: 20px;
+    padding: 10px;
+    overflow: hidden;
+    border-radius: 10px;
+  }
+  .ri-sideMenu_list_title{
+    font-weight: 500;
+    font-size: 18px;
+  }
+  .ri-sideMenu_list_note{
+    font-weight: 400;
+    font-size: 15px;
+    margin-bottom: 10px;
+  }
+  .ri-sideMenu_list_address,.ri-sideMenu_list_openTime,.ri-sideMenu_list_phone{
+    font-weight: 300;
+    font-size: 14px;
+    color: rgb(75, 74, 74);
+    margin-bottom: 5px;
+    span{
+      display: block;
+      font-weight: 400;
+      color: #222;
+    }
+  }
+}
+
 .ri-popup{
   font-family:  'Noto Sans TC',  "Segoe UI", Roboto, "Helvetica Neue", Arial;
   .ri-popup_title{
